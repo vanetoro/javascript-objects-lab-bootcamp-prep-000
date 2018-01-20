c4e8a00e7e22c49daabeaf0dbdd38c9774839420
Vanessa Toro <vanetoro84@gmail.com>
2018-01-20 18:10:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ var recipes = {};
 function updateObjectWithKeyAndValue(obj, key, value){
   var obj2 = Object.assign({}, obj);
   obj2[key] = value;
-  return obj2
+  return obj2;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key]= value;
+  return obj;
 }
